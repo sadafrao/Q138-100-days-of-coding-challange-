1b722f61092218072a35545d6271ca67a43d36a5
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-16 17:58:33 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q138       Day 46 100-days-of-coding-challange
+// sample code snippet for setting a breakpoint
+console.log("Hello World!");
+// set a code using browser developer tool
+console.log("this line has a breakpoint set on its broweser develpor tool");
+console.log("After breakpoint");
